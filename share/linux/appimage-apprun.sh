310a096b19bcd7291cdae7808dec8b223319425e
Janek Bevendorff <janek@keepassxc.org>
2025-11-15 17:43:38 +0100

    Remove LD_LIBRARY_PATH definition

--- share/linux/appimage-apprun.sh
+++ share/linux/appimage-apprun.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
 
 export PATH="$(dirname $0)/usr/bin:${PATH}"
-export LD_LIBRARY_PATH="$(dirname $0)/usr/lib:${LD_LIBRARY_PATH}"
 
 if [ "$1" == "cli" ] || [ "$(basename "$ARGV0")" == "keepassxc-cli" ] || [ "$(basename "$ARGV0")" == "keepassxc-cli.AppImage" ]; then
     [ "$1" == "cli" ] && shift
